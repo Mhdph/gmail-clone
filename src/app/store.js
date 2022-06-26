9d596d4bfab4afc14456d7c82f68fbb99ad29169
Mahdi Parivash <mahdiparivash@protonmail.com>
2022-06-26 19:49:58 +0430

    Remove Code For Refactoring & Create app with vite

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import mailReducer from "../features/counter/MailSlice";
-
-export const store = configureStore({
-  reducer: {
-    mail: mailReducer,
-  },
-});
